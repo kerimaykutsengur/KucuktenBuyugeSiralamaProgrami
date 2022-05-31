c18903e42f1273cbfd5c2123ecc4d1af178df74e
kerimaykutsengur <kaykut23@gmail.com>
2022-05-31 17:31:27 +0300

    Initial commit

---
+++ src/KucuktenBuyugeSiralamaProgrami.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class KucuktenBuyugeSiralamaProgrami {
+    public static void main(String[] args) {
+        int a, b, c;
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("a:");
+        a = input.nextInt();
+        System.out.print("b:");
+        b = input.nextInt();
+        System.out.print("c:");
+        c = input.nextInt();
+
+        if ((a < b) && (a < c)) {
+            if (b < c) {
+                System.out.print("a < b < c");
+            } else  {
+                System.out.print("a < c < b");
+            }
+        } else if ((b < a) && (b < c)) {
+            if (a < c) {
+                System.out.print("b < a < c ");
+            } else {
+                System.out.print("b < c < a");
+            }
+        }else {
+                if (a < b) {
+                    System.out.print("c < a < b");
+                } else if (b < a) {
+                    System.out.print("c < b < a");
+                }
+            }
+
+
+
+
+    }
+}
